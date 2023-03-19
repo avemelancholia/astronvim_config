8d838b225afd494f2d6a9f8cdea03b90dc7af396
Vasily Ivanov <avemelan@pop-os.localdomain>
2023-03-19 17:57:15 +0300

    defaults

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,16 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+   {
+    "ray-x/lsp_signature.nvim",
+    lazy = false,
+    config = function()
+      require("lsp_signature").setup()
+    end,
+   },
+  {
+    "kiyoon/jupynium.nvim",
+    lazy = false,
+    build = "pip3 install --user .",
+  }
 }
